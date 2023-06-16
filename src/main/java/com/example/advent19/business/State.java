2049831ae078340a1398edc1e3303b17c53883a7
Tristan HERSENT <tristan.hersent@epita.fr>
2023-06-16 12:03:06 +0200

    Update

--- src/main/java/com/example/advent19/business/State.java
+++ src/main/java/com/example/advent19/business/State.java
@@ -26,30 +26,57 @@ public class State {
                 obsidianCount, geodeCount, diamondCount, timeLeft);
     }
 
-    public State SimulateBuyingRobotAndIterate(ResourceType resourceType, BuyCosts buyCosts) {
+    public State simulateBuyingRobotAndIterate(ResourceType resourceType, BuyCosts buyCosts) {
         State newState = this.duplicate();
+        if (resourceType == null) {
+            newState.setOreCount(this.getOreCount() + this.getOreRobotCount());
+            newState.setClayCount(this.getClayCount() + this.getClayRobotCount());
+            newState.setObsidianCount(this.getObsidianCount() + this.getObsidianRobotCount());
+            newState.setGeodeCount(this.getGeodeCount() + this.getGeodeRobotCount());
+            newState.setDiamondCount(this.getDiamondCount() + this.getDiamondRobotCount());
+            newState.setTimeLeft(this.getTimeLeft() - 1);
+            return newState;
+        }
         switch (resourceType) {
             case ORE:
                 newState.setOreRobotCount(this.getOreRobotCount() + 1);
                 newState.setOreCount(newState.getOreCount() - buyCosts.getOreRobotCost());
+                newState.setClayCount(this.getClayCount() + this.getClayRobotCount());
+                newState.setObsidianCount(this.getObsidianCount() + this.getObsidianRobotCount());
+                newState.setGeodeCount(this.getGeodeCount() + this.getGeodeRobotCount());
+                newState.setDiamondCount(this.getDiamondCount() + this.getDiamondRobotCount());
                 break;
             case CLAY:
                 newState.setClayRobotCount(this.getClayRobotCount() + 1);
-                newState.setClayCount(newState.getClayCount() - buyCosts.getClayRobotCost());
+                newState.setClayCount(this.getClayCount() - buyCosts.getClayRobotCost());
+                newState.setOreCount(this.getOreCount() + this.getOreRobotCount());
+                newState.setObsidianCount(this.getObsidianCount() + this.getObsidianRobotCount());
+                newState.setGeodeCount(this.getGeodeCount() + this.getGeodeRobotCount());
+                newState.setDiamondCount(this.getDiamondCount() + this.getDiamondRobotCount());
                 break;
             case OBSIDIAN:
                 newState.setObsidianRobotCount(this.getObsidianRobotCount() + 1);
                 newState.setOreCount(this.getOreCount() - buyCosts.getOreRobotCost() + this.getOreCount());
                 newState.setClayCount(this.getClayCount() - buyCosts.getObsidianClayRobotCost() + this.getClayCount());
                 newState.setObsidianCount(this.getObsidianCount() + this.getObsidianRobotCount());
+                newState.setGeodeCount(this.getGeodeCount() + this.getGeodeRobotCount());
+                newState.setDiamondCount(this.getDiamondCount() + this.getDiamondRobotCount());
                 break;
             case GEODE:
                 newState.setGeodeRobotCount(this.getGeodeRobotCount() + 1);
-
+                newState.setOreCount(this.getOreCount() - buyCosts.getOreRobotCost() + this.getOreRobotCount());
+                newState.setClayCount(this.getClayCount() + this.getClayRobotCount());
+                newState.setObsidianCount(this.getObsidianCount() - buyCosts.getGeodeObsidianRobotCost() + this.getObsidianRobotCount());
+                newState.setGeodeCount(this.getGeodeCount() + this.getGeodeRobotCount());
+                newState.setDiamondCount(this.getDiamondCount() + this.getDiamondRobotCount());
                 break;
             case DIAMOND:
                 newState.setDiamondRobotCount(this.getDiamondRobotCount() + 1);
-                newState.setDiamondCount(this.getDiamondCount() - 1);
+                newState.setClayCount(this.getClayCount() - buyCosts.getDiamondClayRobotCost() + this.getClayRobotCount());
+                newState.setObsidianCount(this.getObsidianCount() - buyCosts.getDiamondObsidianRobotCost() + this.getObsidianRobotCount());
+                newState.setGeodeCount(this.getGeodeCount() - buyCosts.getDiamondGeodeRobotCost() + this.getGeodeRobotCount());
+                newState.setDiamondCount(this.getDiamondCount() + this.getDiamondRobotCount());
+                newState.setOreCount(this.getOreCount() + this.getOreRobotCount());
                 break;
         }
         newState.setTimeLeft(this.getTimeLeft() - 1);
